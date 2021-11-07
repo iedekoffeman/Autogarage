77772121649d2d9baeb86f72279f2bda6fc8db66
iedekoffeman <i.koffeman@gmail.com>
2021-11-07 20:47:51 +0100

    Added CustomerRepository

---
+++ src/main/java/nl/novi/autogarage/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package nl.novi.autogarage.repository;
+
+import nl.novi.autogarage.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+}
