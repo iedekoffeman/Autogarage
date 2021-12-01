364aeccdf73c47f4869767f33478aa8064f7c148
iedekoffeman <i.koffeman@gmail.com>
2021-12-01 20:47:55 +0100

    added data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO customers (firstname, lastname)
+VALUES
+('Iede', 'Koffeman'),
+('Pieter', 'Post'),
+('Sandra', 'van Schelt');
+
+INSERT INTO cars (licenseplate)
+VALUES
+    ('24-XZ-ZG'),
+    ('GT-512-Z'),
+    ('XB-392-J');
+
