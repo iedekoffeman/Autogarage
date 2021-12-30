f88612c74f2ee7857ed866c7c7c8bbe3f8e3fd21
iedekoffeman <i.koffeman@gmail.com>
2021-12-30 20:30:09 +0100

    Implemented Repair service class

--- src/main/java/nl/novi/autogarage/service/RepairService.java
+++ src/main/java/nl/novi/autogarage/service/RepairService.java
@@ -1,11 +1,14 @@
 package nl.novi.autogarage.service;
 
+import nl.novi.autogarage.dto.RepairRequestDto;
+import nl.novi.autogarage.exception.RecordNotFoundException;
 import nl.novi.autogarage.model.Repair;
 import nl.novi.autogarage.repository.RepairRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Service
 public class RepairService {
@@ -21,5 +24,74 @@ public class RepairService {
             return repairRepository.findAllByAppointmentDate(appointmentDate);
         }
 
+    }
+
+    public Repair getRepair(int id) {
+        Optional<Repair> optionalRepair = repairRepository.findById(id);
+
+        if(optionalRepair.isPresent()) {
+            return optionalRepair.get();
+        } else {
+            throw new RecordNotFoundException("A repair with ID " + id + " does not exist.");
+        }
+    }
+
+    public void deleteRepair(int id) {
+        repairRepository.deleteById(id);
+    }
+
+    public int addRepair(RepairRequestDto repairRequestDto) {
+
+        Repair repair = new Repair();
+        repair.setAppointmentDate(repairRequestDto.getAppointmentDate());
+        repair.setAppointmentStatus(repairRequestDto.getAppointmentStatus());
+        Repair newRepair = repairRepository.save(repair);
+        return newRepair.getId();
+    }
+
+    public void updateInspection(int id, RepairRequestDto repairRequestDto) {
+
+        Optional<Repair> optionalRepair = repairRepository.findById(id);
+
+        if(optionalRepair.isPresent()) {
+
+            Repair repair = optionalRepair.get();
+            repair.setAppointmentDate(repairRequestDto.getAppointmentDate());
+            repair.setAppointmentStatus(repairRequestDto.getAppointmentStatus());
+            repairRepository.save(repair);
+
+        } else {
+
+            throw new RecordNotFoundException("A repair with ID " + id + " does not exist.");
+        }
+
+
+    }
+    public void partialUpdateRepair(int id, RepairRequestDto repairRequestDto) {
+
+        Optional<Repair> optionalRepair = repairRepository.findById(id);
+
+        if(optionalRepair.isPresent()) {
+
+            Repair repair = optionalRepair.get();
+
+            if(repairRequestDto.getAppointmentDate() != null) {
+
+                repair.setAppointmentDate(repairRequestDto.getAppointmentDate());
+
+            }
+
+            if(repairRequestDto.getAppointmentStatus() != null) {
+
+                repair.setAppointmentStatus(repairRequestDto.getAppointmentStatus());
+
+            }
+
+            repairRepository.save(repair);
+
+        } else {
+
+            throw new RecordNotFoundException("A repair with ID " + id + " does not exist.");
+        }
     }
 }
