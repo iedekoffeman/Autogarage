86f69e0e61f39f1d08ec07529762be9aeb57dede
iedekoffeman <i.koffeman@gmail.com>
2021-12-30 20:17:12 +0100

    Implemented RepairRequestDto

--- src/main/java/nl/novi/autogarage/dto/RepairRequestDto.java
+++ src/main/java/nl/novi/autogarage/dto/RepairRequestDto.java
@@ -1,10 +1,27 @@
 package nl.novi.autogarage.dto;
 
+import nl.novi.autogarage.model.AppointmentStatus;
+
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public class RepairRequestDto {
 
+    @Future(message = "{error.appointmentDate.notInPast}")
+    @NotNull(message = "{error.appointmentDate.notnull}" )
     private LocalDate appointmentDate;
+
+    @NotNull(message = "{error.appointmentStatus.notnull}")
+    private AppointmentStatus appointmentStatus;
+
+    public AppointmentStatus getAppointmentStatus() {
+        return appointmentStatus;
+    }
+
+    public void setAppointmentStatus(AppointmentStatus appointmentStatus) {
+        this.appointmentStatus = appointmentStatus;
+    }
 
     public LocalDate getAppointmentDate() {
         return appointmentDate;
