3dde548c33baedfdc50031f5563928cf4ac359c3
iedekoffeman <i.koffeman@gmail.com>
2021-12-31 09:34:21 +0100

    Implemented item repository

--- src/main/java/nl/novi/autogarage/repository/ItemRepository.java
+++ src/main/java/nl/novi/autogarage/repository/ItemRepository.java
@@ -1,4 +1,11 @@
 package nl.novi.autogarage.repository;
 
-public interface ItemRepository {
+import nl.novi.autogarage.model.Deficiency;
+import nl.novi.autogarage.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Integer> {
+
 }
