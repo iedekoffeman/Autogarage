767985ba23ebccf541311574e275c0c49cdb0317
iedekoffeman <i.koffeman@gmail.com>
2022-01-02 22:38:50 +0100

    Changed hibernate ddl auto setting to create-drop because tests failed

--- src/test/java/nl/novi/autogarage/AutogarageApplicationTests.java
+++ src/test/java/nl/novi/autogarage/AutogarageApplicationTests.java
@@ -1,24 +1,14 @@
 package nl.novi.autogarage;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-@ContextConfiguration(classes={AutogarageApplication.class})
 class AutogarageApplicationTests {
 
     @Test
-    @DisplayName("Testing if context is correctly set")
-    void contextLoads(ApplicationContext context) {
-
-        assertNotNull(context);
-
-
+    void contextLoads() {
     }
 
 }
