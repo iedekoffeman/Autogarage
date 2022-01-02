b077650f1b3b104de627ad342739c4e849eef2d6
iedekoffeman <i.koffeman@gmail.com>
2022-01-02 21:46:56 +0100

    Renamed the customer tests (Integration/End-to-end) because the names were mixed up

--- src/test/java/nl/novi/autogarage/end_to_end/CustomerTest.java
+++ src/test/java/nl/novi/autogarage/end_to_end/CustomerTest.java
@@ -1,58 +1,46 @@
-package nl.novi.autogarage.controller;
+package nl.novi.autogarage.end_to_end;
+
 
 import nl.novi.autogarage.AutogarageApplication;
-import nl.novi.autogarage.model.Customer;
-import nl.novi.autogarage.security.JwtUtil;
-import nl.novi.autogarage.service.CustomerService;
+
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Arrays;
-import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@WebMvcTest(CustomerController.class)
-@ContextConfiguration(classes={AutogarageApplication.class})
+
+@SpringBootTest
+@AutoConfigureMockMvc
 @EnableConfigurationProperties
-public class CustomerControllerIntegrationTest {
+public class CustomerTest {
 
     @Autowired
     private MockMvc mvc;
 
-    @MockBean
-    private JwtUtil jwtUtil;
-
-    @MockBean
-    private CustomerService service;
-
     @Test
-    public void testEndpointGetAllCustomers() throws Exception {
-
-        Customer customer = new Customer();
-        customer.setFirstname("John");
-        customer.setLastname("Doe");
-        List<Customer> allCustomers = Arrays.asList(customer);
-
-        given(service.getAllCustomers()).willReturn(allCustomers);
+    public void testEndpointCustomers() throws Exception {
 
         mvc.perform(get("/api/v1/customers")
+                        .with(user("user").roles("USER"))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].lastname", is("Doe")));
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[0].lastname", is("Koffeman")));
 
     }
 
