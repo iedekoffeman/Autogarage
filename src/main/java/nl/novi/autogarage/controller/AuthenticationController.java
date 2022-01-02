1853dac87000593451174dc3accb0f61b5aa281c
iedekoffeman <i.koffeman@gmail.com>
2022-01-02 22:05:12 +0100

    Refactored authentication endpoints urls and method names

--- src/main/java/nl/novi/autogarage/controller/AuthenticationController.java
+++ src/main/java/nl/novi/autogarage/controller/AuthenticationController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/api/v1/authenticate")
 public class AuthenticationController {
 
     UserAuthenticateService userAuthenticateService;
@@ -20,7 +22,7 @@ public class AuthenticationController {
         this.userAuthenticateService = userAuthenticateService;
     }
 
-    @PostMapping(value = "/authenticate")
+    @PostMapping(value = "")
     public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequestDto authenticationRequestDto) {
 
         AuthenticationResponseDto authenticationResponseDto = userAuthenticateService.authenticateUser(authenticationRequestDto);
