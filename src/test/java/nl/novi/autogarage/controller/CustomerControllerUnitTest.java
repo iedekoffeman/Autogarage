058d27e687011121545a6a8a310a9505fc56bc95
iedekoffeman <i.koffeman@gmail.com>
2022-01-05 14:59:55 +0100

    Added createCustomer controller unit test

--- src/test/java/nl/novi/autogarage/controller/CustomerControllerUnitTest.java
+++ src/test/java/nl/novi/autogarage/controller/CustomerControllerUnitTest.java
@@ -1,10 +1,15 @@
 package nl.novi.autogarage.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import nl.novi.autogarage.AutogarageApplication;
+import nl.novi.autogarage.dto.CustomerRequestDto;
 import nl.novi.autogarage.model.Customer;
 import nl.novi.autogarage.security.JwtUtil;
 import nl.novi.autogarage.service.CustomerService;
+import org.apache.catalina.mapper.Mapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -12,6 +17,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,7 +25,10 @@ import java.util.List;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -38,6 +47,7 @@ public class CustomerControllerUnitTest {
     @MockBean
     private CustomerService customerService;
 
+
     @Test
     public void testEndpointGetAllCustomers() throws Exception {
 
@@ -55,5 +65,31 @@ public class CustomerControllerUnitTest {
                 .andExpect(jsonPath("$[0].lastname", is("Doe")));
 
     }
+    @Test
+    public void testCreateCustomer() throws Exception {
+
+        CustomerRequestDto customerRequestDto = new CustomerRequestDto();
+        customerRequestDto.setFirstname("Iede");
+        customerRequestDto.setLastname("Koffeman");
+
+        Customer customer = new Customer();
+        customer.setFirstname(customerRequestDto.getFirstname());
+        customer.setLastname(customerRequestDto.getLastname());
+
+        mvc.perform(post("/api/v1/customers")
+                .content(asJsonString(customerRequestDto))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
 }
